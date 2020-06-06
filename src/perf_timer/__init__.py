13eacc0e2c57b4c76ff50dcbdc96e25c2d1d4844
John Belmonte <john@neggie.net>
2020-06-06 13:07:59 +0900

    expose docs to help()

--- src/perf_timer/__init__.py
+++ src/perf_timer/__init__.py
@@ -4,3 +4,15 @@ try:
 except ImportError:
     pass
 from ._version import __version__
+
+def _metadata_fix():
+    # don't do this for Sphinx case because it breaks "bysource" member ordering
+    import sys  # pylint: disable=import-outside-toplevel
+    if 'sphinx' in sys.modules:
+        return
+
+    for name, value in globals().items():
+        if not name.startswith('_'):
+            value.__module__ = __name__
+
+_metadata_fix()
